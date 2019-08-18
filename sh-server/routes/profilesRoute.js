8bb568d655fc71263f17492ca7eddc751d60a21f
Asaf Benjaminov <asaf.benjaminov@gameffective.com>
2019-08-18 13:31:21 +0300

    profiles

--- sh-server/routes/profilesRoute.js
+++ sh-server/routes/profilesRoute.js
@@ -2,9 +2,43 @@ var express = require('express');
 var router = express.Router();
 
 var profiles = [];
+var profileId = 0;
 
-profiles.push({ id:'0', name: "Asaf Benjaminov",professions:[{ id:0, name: "Senior" },{ id:1, name: "Intern" }], profilePic = "" })
-profiles.push({ id:'1' , name: "Israel israeli",professions:[{ id:1, name: "Intern" }], profilePic = "" });
+profiles.push({id: (profileId++).toString(), name: 'בנימינוב',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'ברלוביץ',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'בוגאט',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'שינקמן',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'כאלותי',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'פרידמן',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'פראנק',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'פינקלשטיין',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'ברנס',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'הדס',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'ציטר',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'זארקוב',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'בן דויד',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'זגל',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'ברקוביץ',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'אקרמן',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'סטראנו',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'נקיטין',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'גכטמן',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'פרשטנדיג',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'קניזניק',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'פרקש',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'אפלבוים',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'פרנקל',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'אלפרסון',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'פיצחדזדה',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'ויינברג',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'שחנובסקי',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'יופייטה',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'אלטורה',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'מהאני',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'שאהין',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'צאלחי',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'זיד',professions:[],profilePic: ''});
+profiles.push({id: (profileId++).toString(), name: 'נאגאר',professions:[],profilePic: ''});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
--- sh-server/routes/tagsRoute.js
+++ sh-server/routes/tagsRoute.js
@@ -1,7 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-var tags =  [{ id:0, name: "Senior" },{ id:1, name: "Intern" },{ id:2, name: "Senior - Kids" }];
+var tagId = 0;
+
+var tags =  [{ id:tagId++, name: "Senior" },
+  { id:tagId++, name: "Conventional" },
+  { id:tagId++, name: "RF" },
+  { id:tagId++, name: "CT Protocoling" },
+  { id:tagId++, name: "CT ER" },
+  { id:tagId++, name: "Chest" },
+  { id:tagId++, name: "Neuroradiology" },
+  { id:tagId++, name: "US" },
+  { id:tagId++, name: "MRI" },
+  { id:tagId++, name: "Pediatrics" },
+  { id:tagId++, name: "MSK" },
+  { id:tagId++, name: "Breast" },
+  { id:tagId++, name: "Angio" },
+  { id:tagId++, name: "Intern" }];
 
 router.get('/', function(req, res, next) {
   res.json({data : tags});
