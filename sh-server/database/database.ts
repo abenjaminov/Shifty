6a18fb2fd117c367e9636dce97f71336ae1d12b6
Asaf Benjaminov <asaf.benjaminov@gameffective.com>
2019-09-01 11:32:15 +0300

    oneToOne map

--- sh-server/database/database.ts
+++ sh-server/database/database.ts
@@ -137,15 +137,32 @@ export class DbContext {
                 typeToDeepTypes.forEach((deepType, key) => {
                     for(let oneToOneMapping of deepType.oneToOneMappings) {
                         let objects = distinctResult.map(dr => dr[deepType.jsonProperty]);
-                        var ids = objects.map(x => x[deepType.keyDbColumn]);
+                        if(objects.length > 0) {
+                            var ids: any[];
+                            if(Array.isArray(objects[0])) {
+                                ids = objects.map(value => value.map((innerValues: any) => innerValues[deepType.keyDbColumn])).reduce((total: any[], o) => total.concat(o));    
+                            }
+                            else {
+                                ids = objects.map(x => x[deepType.keyDbColumn]);        
+                            }
 
-                        var itemMap = oneToOneMapping.toItemMap(ids, result, deepType.typeAlias);
+                            var itemMap = oneToOneMapping.toItemMap(ids, result, deepType.typeAlias);
 
-                        objects.forEach((resItem: any) => {
-                            var thisItem = itemMap.get(resItem[`${deepType.typeAlias}_${deepType.keyDbColumn}`]);
-    
-                            (resItem as any)[oneToOneMapping.jsonProperty] = thisItem;
-                        })
+                            objects.forEach((resItem: any) => {
+                                if(Array.isArray(resItem)) {
+                                    resItem.forEach((innerResItem: any) => {
+                                        var thisItem = itemMap.get(innerResItem[`${deepType.keyDbColumn}`]);
+        
+                                        (innerResItem as any)[oneToOneMapping.jsonProperty] = thisItem;
+                                    })
+                                }
+                                else {
+                                    var thisItem = itemMap.get(resItem[`${deepType.typeAlias}_${deepType.keyDbColumn}`]);
+        
+                                    (resItem as any)[oneToOneMapping.jsonProperty] = thisItem;
+                                }
+                            })
+                        }
                     }
                 });
 
